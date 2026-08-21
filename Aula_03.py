1bcfbbbba077bca17ee57bde008dd5d6fcfed702
Luciano <Lucianoferraz1331@gmail.com>
2026-08-21 20:12:26 -0300

    Aula 03 - Final

--- Aula_03.py
+++ Aula_03.py
@@ -9,8 +9,6 @@ class Node():
         Peso = 0
         Qtd = 0
 
-
-
         while atual != None:
             Peso += atual.peso
             Qtd += 1
@@ -25,19 +23,56 @@ class Node():
 
         print(f"Peso total do trem: {Peso}")
         print(f"Quantidade de vagões: {Qtd}")
-            
+
+    def desengatar_vagao(primeiro_no, nome_alvo):
+        atual = primeiro_no
+        anterior = None
+
+        while atual != None:
+            if atual.nome == nome_alvo:
+                if anterior is None:
+                    return atual.proximo
+                else:
+                    anterior.proximo = atual.proximo
+                    return primeiro_no
+
+            anterior = atual
+            atual = atual.proximo
+
+    def inverter_trem(primeiro_no):
+        anterior = None
+        atual = primeiro_no
+
+        while atual is not None:
+            proximo = atual.proximo
+            atual.proximo = anterior
+            anterior = atual
+            atual = proximo
+
+        return anterior
 
 no1 = Node("Locomotiva", 80, None)
 no2 = Node("Vagão Carga", 50, None)
 no3 = Node("Vagão Passageiros", 30, None)
 no4 = Node("Vagão Cauda", 10, None)
+restaurante = Node("Vagão Restaurante", 25.0, None)
 
 primeiro = no1
 
 no1.proximo = no2
-no2.proximo = no3
+no2.proximo = restaurante
+restaurante.proximo = no3
 no3.proximo = no4
 
+print(Node.relatorio_trem(no1))
+
+
+
+print(Node.relatorio_trem(no1))
+
+primeiro = Node.desengatar_vagao(no1, "Vagão Carga")
+
+print(Node.relatorio_trem(no1))
+
+primeiro = Node.inverter_trem(primeiro)
 print(Node.relatorio_trem(primeiro))
-
-
